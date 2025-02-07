37a0e3f1806bcab6859d18c92dd4fa96dcfb1e96
drimag <rafaelvedimagiba@gmail.com>
2025-02-07 14:45:35 +0800

    is prime func

--- A1B1/main.cpp
+++ A1B1/main.cpp
@@ -7,10 +7,37 @@
 #include <chrono>
 #include <ctime>
 
+bool isPrime(int n)
+{
+    if (n <= 1)
+    {
+        return false;
+    }
+    if (n == 2)
+    {
+        return true;
+    }
+    if (n % 2 == 0)
+    {
+        return false;
+    }
+
+    for (int i = 3; i < n; i += 2)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int main()
 {
     int x, y;
     std::ifstream config("config.txt");
+
     if (!(config >> x >> y))
     {
         std::cerr << "Error reading config file.\n";
@@ -18,14 +45,21 @@ int main()
     }
 
     auto now = std::chrono::system_clock::now();
-
-    // Convert the time point to a time_t, which represents the time in seconds since the epoch
     std::time_t now_time_t = std::chrono::system_clock::to_time_t(now);
-
-    // Convert the time_t to a tm structure for local time
     std::tm now_tm = *std::localtime(&now_time_t);
+    std::cout << x << " " << y << std::put_time(&now_tm, " %Y-%m-%d %H:%M:%S") << "\n";
 
-    std::cout << x << " " << y << std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S") << "\n";
+    if (isPrime(x))
+    {
+        auto now = std::chrono::system_clock::now();
+        std::time_t now_time_t = std::chrono::system_clock::to_time_t(now);
+        std::tm now_tm = *std::localtime(&now_time_t);
+
+        // TODO: include thread no.
+        std::cout << x << " is a prime number " << std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S") << "\n";
+    }
+
+    system("pause");
 
     return 0;
 }
