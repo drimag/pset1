4ba6841783b815e7cafe8eb55507075cf51b0cb9
Rafael Dimagiba <rafael_dimagiba@dlsu.edu.ph>
2025-02-07 10:00:05 +0800

    timestamp

--- A1B1/main.cpp
+++ A1B1/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <thread>
 #include <mutex>
+#include <iomanip>
 #include <chrono>
 #include <ctime>
 
@@ -16,7 +17,15 @@ int main()
         return 1;
     }
 
-    std::cout << x << " " << y << "\n";
+    auto now = std::chrono::system_clock::now();
+
+    // Convert the time point to a time_t, which represents the time in seconds since the epoch
+    std::time_t now_time_t = std::chrono::system_clock::to_time_t(now);
+
+    // Convert the time_t to a tm structure for local time
+    std::tm now_tm = *std::localtime(&now_time_t);
+
+    std::cout << x << " " << y << std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S") << "\n";
 
     return 0;
 }
